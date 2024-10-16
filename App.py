b08169771d1d8bb8c48adaef907c841c4523f7d2
sarubala <146551111+sarubala22@users.noreply.github.com>
2024-10-16 23:43:13 +0530

    Update App.py

--- App.py
+++ App.py
@@ -42,7 +42,7 @@ from Courses import ds_course,web_course,android_course,ios_course,uiux_course,r
 import pafy #for uploading youtube videos
 import plotly.express as px #to create visualisations at the admin session
 import nltk
-nltk.download('stopwords')
+nltk.download('stopwords' , download_dir='C:\Users\saru5\anaconda3\pkgs\nltk-3.8.1-py312haa95532_0\Scripts')
 
 
 def fetch_yt_video(link):
